1a7a1632d0d331ec706b4aa87f2b07d9f335cc9f
Tzeng Yuxio <tzengyuxio@gmail.com>
2023-06-23 00:06:02 +0800

    chore: refine configs of editorconfig, prettier, eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-	root: true,
-	env: {
-		browser: true, es2021: true,
-	},
-	extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier/skip-formatting', 'xo'],
-	overrides: [],
-	parserOptions: {
-		ecmaVersion: 'latest', sourceType: 'module',
-	},
-	plugins: ['vue'],
-	rules: {
-		'no-bitwise': 'off',
-	},
+  root: true,
+  env: { browser: true, es2021: true, node: true },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier/skip-formatting",
+    "prettier",
+  ],
+  overrides: [],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["vue", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "no-bitwise": "off",
+  },
 };
